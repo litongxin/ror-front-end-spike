8a2443937b1e68acca58684622b6b8bdded5d966
Li Tongxin <tomboy17@163.com>
2016-03-20 20:33:55 +0800

    Refactor the LocationItem and enable the proptypes validation.

--- src/js/components/LocationItem.js
+++ src/js/components/LocationItem.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
 export default class LocationItem extends React.Component {
   render() {
+    const { location } = this.props
+    const name = location.name
+    const description = location.description
     return (
       <li>
-        <h3 className="location-name">{this.props.location.name}</h3>
-        <p className="location-desc">{this.props.location.description}</p>
+        <h3 className="location-name">{name}</h3>
+        <p className="location-desc">{description}</p>
       </li>
     );
   }
+}
+
+LocationItem.propTypes = {
+  location: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired
 }
