7b7b007c6e8e0174c8f4eac58e971d31ae6210cd
Li Tongxin <tomboy17@163.com>
2016-03-20 23:29:56 +0800

    Split configureStore into two file to enable devtools.

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,25 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
-import api from '../middleware/api'
-import rootReducer from '../reducers'
-
-export default function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(thunk, api, createLogger())
-    )
-  )
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').default
-      store.replaceReducer(nextRootReducer)
-    })
-  }
-
-  return store
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod')
+} else {
+  module.exports = require('./configureStore.dev')
 }
