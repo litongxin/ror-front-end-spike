0ad33691b2143479fb0c393f2451946d3514ca83
Li Tongxin <tomboy17@163.com>
2016-03-21 00:58:14 +0800

    Enable devtools in Root componment and make it finally works.

--- src/js/containers/Root.js
+++ src/js/containers/Root.js
@@ -1,22 +1,5 @@
-import React, { Component, PropTypes } from 'react'
-import { Provider } from 'react-redux'
-import routes from '../routes'
-import { Router } from 'react-router'
-
-export default class Root extends Component {
-  render() {
-    const { store, history } = this.props
-    return (
-      <Provider store={store}>
-        <div>
-          <Router history={history} routes={routes} />
-        </div>
-      </Provider>
-    )
-  }
-}
-
-Root.propTypes = {
-  store: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./Root.prod')
+} else {
+  module.exports = require('./Root.dev')
 }
